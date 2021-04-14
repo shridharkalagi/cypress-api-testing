87ae2e35c66c9dc5b98ed624d074ff7a218ba925
shridharkalagi <shridharkalagi@gmail.com>
2021-04-14 18:36:41 +0530

    first commit

---
+++ cypress/integration/sampleTest/sample.js
@@ -0,0 +1,5 @@
+describe("Marvel characters", () => {
+  it("Get all Marvel characters", () => {
+    cy.request("www.google.com"); // We will build the URL and test assertions in the coming steps
+  });
+});
